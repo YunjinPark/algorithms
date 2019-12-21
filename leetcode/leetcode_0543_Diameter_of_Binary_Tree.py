8f90a33abf451f2490f1d8d9993b64d60ef0bed0
YunjinPark <yjseraphina89@gmail.com>
2019-12-21 14:09:06 +0900

    -

--- leetcode/leetcode_0543_Diameter_of_Binary_Tree.py
+++ leetcode/leetcode_0543_Diameter_of_Binary_Tree.py
@@ -10,11 +10,12 @@ class Solution:
         self.a = 1
 
         def depth(node: TreeNode):
-            if node == None: return 0
+            if node: return 0
             l = depth(node.left)
             r = depth(node.right)
             self.a = max(self.a, l + r + 1)
             return max(l, r) + 1
 
+        depth(root)
         depth(root)
         return self.a - 1
